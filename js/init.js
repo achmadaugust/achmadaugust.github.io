4c00ff4c87a2d783edec7071575e1e2cc36fde08
AT <AT@AT-THINK>
2017-11-04 14:46:02 +0700

    First commit web profile

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+  	// $('.dropdown-button').dropdown();
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
